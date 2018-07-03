a8b3eeb46e1d7695c3d7e5dff686928c77eeaa17
kkaris <karis.klas@gmail.com>
2018-07-03 18:49:07 -0400

    Genes allowed in interactions can now be filtered on provided gene set file to different degrees.

--- depmap_script.py
+++ depmap_script.py
@@ -3,10 +3,27 @@ import argparse as ap
 import pandas as pd
 import depmap_network_functions as dnf
 from time import time
+import pdb
 
 # Temp fix because indra doesn't import when script is run from terminal =
 import sys
 sys.path.append('~/repos/indra/')
+
+# 1. no geneset -> use corr from full DepMap data, no filtering needed
+# 2. geneset, not strict -> interaction has to contain at least one gene from
+#    the loaded list
+# 3. geneset loaded, strict -> each correlation can only contain genes from
+#    the loaded data set
+
+
+def read_gene_set_file(gf, data):
+    gset = []
+    with open(gf, 'rt') as f:
+        for g in f.readlines():
+            gn = g.upper().strip()
+            if gn in data:
+                gset.append(gn)
+    return gset
 
 
 def main(args):
@@ -15,29 +32,40 @@ def main(args):
     data = pd.read_csv(args.ceres_file, index_col=0, header=0)
     data = data.T
 
-    # Calculate the correlations, or load from cached
-    if args.recalc:
-        corr = data.corr()
-        corr.to_hdf('correlations.h5', 'correlations')
-    else:
-        corr = pd.read_hdf('correlations.h5', 'correlations')
+    # pdb.set_trace()  # Check if 'data' is ok; check args.geneset_file args.strict 
+    if args.geneset_file:
+        # Read gene set to look at
+        gene_set = read_gene_set_file(gf=args.geneset_file, data=data)
+        # pdb.set_trace()  # Check if there is a 'gene_set'
 
-    # Read gene set to look at
-    gene_set = []
-    with open(args.geneset_file, 'rt') as f:
-        for line in f.readlines():
-            gene_name = line.strip().upper()
-            if gene_name in data:
-                gene_set.append(gene_name)
+    # 1. no loaded gene list OR 2. loaded gene list but not strict -> data.corr
+    if not args.geneset_file or (args.geneset_file and not args.strict):
+        # Calculate the full correlations, or load from cached
+        if args.recalc:
+            corr = data.corr()
+            corr.to_hdf('correlations.h5', 'correlations')
+        else:
+            corr = pd.read_hdf('correlations.h5', 'correlations')
+        # pdb.set_trace()  # Check corr
+        # No gene set file, leave 'corr' intact and unstack
+        if not args.geneset_file:
+            fcorr_list = corr.unstack()
 
-    filtr_data = data[gene_set]  # ToDo: filter one gene in pair is in set
-    filtr = filtr_data.corr()
-    fcorr_list = filtr.unstack()
+        # Gene set file present: filter and unstack
+        elif args.geneset_file and not args.strict:
+            fcorr_list = corr[gene_set].unstack()
+            # pdb.set_trace()  # check fcorr_list
+
+    # 3. Strict: both genes in interaction must be from loaded set
+    elif args.geneset_file and args.strict:
+        fcorr_list = data[gene_set].corr().unstack()
+
     flarge_corr = fcorr_list[fcorr_list != 1.0]
     flarge_corr = flarge_corr[flarge_corr.abs() > args.ll]
     if args.ul < 1.0:
         flarge_corr = flarge_corr[flarge_corr.abs() < args.ul]
     fsort_corrs = flarge_corr.abs().sort_values(ascending=False)
+    # pdb.set_trace()  # Check fsort_corrs
 
     # Find out if the HGNC pairs are connected and if they are how
     uniq_pairs = set()
@@ -69,13 +97,20 @@ if __name__ == '__main__':
     parser = ap.ArgumentParser()
     parser.add_argument('-f', '--ceres-file', required=True,
                         help='Ceres correlation score in csv format')
-    parser.add_argument('-g', '--geneset-file', required=True,
-                        help='File with gene set data.')
+    parser.add_argument('-g', '--geneset-file',
+                        help='Filter to interactions with gene set data file.')
+    parser.add_argument('-s', '--strict', action='store_true',
+                        help='With \'-s\', the correlations are restricted to '
+                             'only be between loaded gene set. If no gene set '
+                             'is loaded, this option has no effect.')
+    parser.add_argument('-stf', '--statement-file',
+                        help='With \'-stf\' use file with INDRA statements '
+                             'instead of quering from the web clients.')
     parser.add_argument('-o', '--outbasename', default=str(int(time())),
                         help='Base name for outfiles. Default: UTC timestamp')
     parser.add_argument('-r', '--recalc', action='store_true',
-                        help='If True (Default), recalculate correlation '
-                             'calculation.')
+                        help='With \'-r\', recalculate full gene-gene '
+                             'correlation of data set.')
     parser.add_argument('-ll', type=float, default=0.5,
                         help='Lower limit CERES correlation score filter.')
     parser.add_argument('-ul', type=float, default=1.0,
