d733a41238283d5b648dc7a57744f686d9610523
kkaris <karis.klas@gmail.com>
2018-07-18 17:19:09 -0400

    Cleaned up after moving get_correlations to depmap_network_functions.

--- depmap_script.py
+++ depmap_script.py
@@ -21,16 +21,6 @@ logger = logging.getLogger('depmap_script')
 #    the loaded data set
 
 
-def _read_gene_set_file(gf, data):
-    gset = []
-    with open(gf, 'rt') as f:
-        for g in f.readlines():
-            gn = g.upper().strip()
-            if gn in data:
-                gset.append(gn)
-    return gset
-
-
 def _is_float(n):
     if type(n) is np.float64 or type(n) is float:
         return True
@@ -38,78 +28,14 @@ def _is_float(n):
         return False
 
 
-def get_correlations(ceres_file, geneset_file, corr_file, strict, outbasename,
-                     recalc=False, lower_limit=0.3, upper_limit=1.0):
-    # Open data file
-    logger.info("Reading CERES data from %s" % ceres_file)
-    data = pd.read_csv(ceres_file, index_col=0, header=0)
-    data = data.T
-
-    if geneset_file:
-        # Read gene set to look at
-        gene_filter_list = _read_gene_set_file(gf=geneset_file, data=data)
-
-    # 1. no loaded gene list OR 2. loaded gene list but not strict -> data.corr
-    if not geneset_file or (geneset_file and not strict):
-        # Calculate the full correlations, or load from cached
-        if recalc:
-            logger.info("Calculating correlations (may take a long time)")
-            corr = data.corr()
-            corr.to_hdf('correlations.h5', 'correlations')
-        else:
-            logger.info("Loading correlations from %s" % corr_file)
-            corr = pd.read_hdf(corr_file, 'correlations')
-        # No gene set file, leave 'corr' intact and unstack
-        if not geneset_file:
-            fcorr_list = corr.unstack()
-
-        # Gene set file present: filter and unstack
-        elif geneset_file and not strict:
-            fcorr_list = corr[gene_filter_list].unstack()
-
-    # 3. Strict: both genes in interaction must be from loaded set;
-    #    Filter data, then calculate correlations and then unstack
-    elif geneset_file and strict:
-        fcorr_list = data[gene_filter_list].corr().unstack()
-
-    # Remove self correlation, correlations below ll, sort on magnitude,
-    # leave correlation intact
-    logger.info("Removing self correlations")
-    flarge_corr = fcorr_list[fcorr_list != 1.0] # Self correlations
-    logger.info("Filtering correlations to %.1f < C < %.1f" %
-                (lower_limit, upper_limit))
-    flarge_corr = flarge_corr[flarge_corr.abs() > lower_limit]
-    if upper_limit < 1.0:
-        flarge_corr = flarge_corr[flarge_corr.abs() < upper_limit]
-
-    # Sort by absolute value
-    logger.info("Sorting correlations by absolute value")
-    fsort_corrs = flarge_corr[
-        flarge_corr.abs().sort_values(ascending=False).index]
-
-    # Compile set of correlations to be explained without duplicates A-B, B-A
-    logger.info("Compiling deduplicated set of correlations")
-    all_hgnc_ids = set()
-    uniq_pairs = []
-    with open(outbasename+'_all.csv', 'w', newline='') as csvf:
-        wrtr = csv.writer(csvf, delimiter=',')
-        for pair in fsort_corrs.items():
-            (id1, id2), correlation = pair
-            if (id2, id1, correlation) not in uniq_pairs:
-                uniq_pairs.append((id1, id2, correlation))
-                all_hgnc_ids.update([id1, id2])
-                wrtr.writerow([id1, id2, correlation])
-    return gene_filter_list, uniq_pairs, all_hgnc_ids, fsort_corrs
-
-
 def main(args):
 
     # Prepare data
     gene_filter_list, uniq_pairs, all_hgnc_ids, fsort_corrs = \
-            get_correlations(args.ceres_file, args.geneset_file,
-                             args.corr_file, args.strict,
-                             args.outbasename, args.recalc,
-                             args.ll, args.ul)
+            dnf.get_correlations(args.ceres_file, args.geneset_file,
+                                 args.corr_file, args.strict,
+                                 args.outbasename, args.recalc,
+                                 args.ll, args.ul)
 
     # Get statements from file or from database that contain any gene from
     # provided list as set
@@ -160,7 +86,7 @@ def main(args):
                 (nested_dict_statements.get(id2) and
                  nested_dict_statements.get(id2).get(id1)):
             new_pair = r'\section{{{}, {}: {}}}'.format(id1, id2, fmt_corr) \
-                 +'\n'+ \
+                 + '\n' + \
                  r'See correlation plot \href{{' \
                  r'https://depmap.org/portal/interactive/?xDataset=Avana' \
                  r'&xFeature={}&yDataset=Avana&yFeature={}&colorDataset=' \
