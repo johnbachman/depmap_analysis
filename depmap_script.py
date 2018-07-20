303258ae255f4bf9ede39e0632303bf47942ae12
kkaris <karis.klas@gmail.com>
2018-07-20 13:53:31 -0400

    Now checking for paths of len == 2, simplified logic for nest_dict check

--- depmap_script.py
+++ depmap_script.py
@@ -1,10 +1,11 @@
 import csv
 import logging
-import argparse as ap
-from time import time
 import numpy as np
 import pandas as pd
+from time import time
 import networkx as nx
+import argparse as ap
+import itertools as itt
 from indra.tools import assemble_corpus as ac
 import depmap_network_functions as dnf
 
@@ -28,14 +29,30 @@ def _is_float(n):
         return False
 
 
+def _corr_web_latex(id1, id2, fmtcorr):
+    web_text = r'\section{{{}, {}: {}}}'.format(id1, id2, fmtcorr) + '\n' + \
+                 r'See correlation plot \href{{' \
+                 r'https://depmap.org/portal/interactive/?xDataset=Avana' \
+                 r'&xFeature={}&yDataset=Avana&yFeature={}&colorDataset=' \
+                 r'lineage&colorFeature=all&filterDataset=context' \
+                 r'&filterFeature=&regressionLine=false&statisticsTable=false' \
+                 r'&associationTable=true&plotOnly=false}}{{here}}'.format(
+                     id1, id2) + '\n\n'
+
+    return web_text
+
+
 def main(args):
+
+    # Evidence list must have length above this value to be part of the
+    # text output
+    ev_fltr = 0
 
     # Prepare data
     gene_filter_list, uniq_pairs, all_hgnc_ids, fsort_corrs = \
-            dnf.get_correlations(args.ceres_file, args.geneset_file,
-                                 args.corr_file, args.strict,
-                                 args.outbasename, args.recalc,
-                                 args.ll, args.ul)
+        dnf.get_correlations(args.ceres_file, args.geneset_file,
+                             args.corr_file, args.strict, args.outbasename,
+                             args.recalc, args.ll, args.ul)
 
     # Get statements from file or from database that contain any gene from
     # provided list as set
@@ -57,102 +74,119 @@ def main(args):
     # Get nested dicts from statements
     nested_dict_statements = dnf.nested_dict_gen(stmts_all)
 
+    # Get undirected graph from nested dict
+    undir_nx_graph = dnf.nx_undirected_graph_from_nested_dict(
+        nest_d=nested_dict_statements)
+    node_set = set(undir_nx_graph.nodes())
+
     # Loop through the unique pairs
-    dir_conn_pairs = []
-    dir_neg_conn_pairs = []
-    unexplained = []
+    dir_conn_pairs = []  # Save pairs that are directly connected
+    two_step_conn_pairs = []  # Extend to pairs connected by two direct edges
+    dir_neg_conn_pairs = []  # Directly connected pairs with correlation < 0
+    unexplained = []  # Unexplained correlations
     npairs = len(uniq_pairs)
 
     # Open files to write text/latex output
-    f_con = open(args.outbasename + '_connections_latex.tex', 'w')
-    f_neg_c = open(args.outbasename + '_neg_conn_latex.tex', 'w')
+    with open(args.outbasename + '_connections_latex.tex', 'w') as f_con, \
+            open(args.outbasename + '_neg_conn_latex.tex', 'w') as f_neg_c:
 
-    logger.info('Looking for connections between %i pairs' % npairs)
-    for pair in uniq_pairs:
-        pl = list(pair)
-        for li in pl:
-            if _is_float(li):
-                correlation = li
-                fmt_corr = '{0:.04}'.format(correlation)
-                break
-        pl.remove(correlation)
-        id1, id2 = pl
+        logger.info('Looking for connections between %i pairs' % npairs)
+        for pair in uniq_pairs:
+            correlation, fmt_corr = None, None
+            pl = list(pair)
+            for li in pl:
+                if _is_float(li):
+                    correlation = li
+                    fmt_corr = '{0:.04}'.format(correlation)
+                    break
+            pl.remove(correlation)
+            id1, id2 = pl
 
-        forward_fail = False
-        backward_fail = False
+            if (nested_dict_statements.get(id1) and
+                    nested_dict_statements.get(id1).get(id2)) or \
+                    (nested_dict_statements.get(id2) and
+                     nested_dict_statements.get(id2).get(id1)):
+                new_pair = _corr_web_latex(id1, id2, fmt_corr)
+                f_con.write(new_pair)
 
-        if (nested_dict_statements.get(id1) and
-                nested_dict_statements.get(id1).get(id2)) or \
-                (nested_dict_statements.get(id2) and
-                 nested_dict_statements.get(id2).get(id1)):
-            new_pair = r'\section{{{}, {}: {}}}'.format(id1, id2, fmt_corr) \
-                 + '\n' + \
-                 r'See correlation plot \href{{' \
-                 r'https://depmap.org/portal/interactive/?xDataset=Avana' \
-                 r'&xFeature={}&yDataset=Avana&yFeature={}&colorDataset=' \
-                 r'lineage&colorFeature=all&filterDataset=context' \
-                 r'&filterFeature=&regressionLine=false&statisticsTable=false' \
-                 r'&associationTable=true&plotOnly=false}}{{here}}'.format(
-                     id1, id2) + '\n\n'
-            f_con.write(new_pair)
-            if correlation < 0:
-                f_neg_c.write(new_pair)
+                if correlation < 0:
+                    f_neg_c.write(new_pair)
 
-        # nested_dict_statements.get(id1).get(id2) raises AttributeError
-        # if nested_dict_statements.get(id1) returns {}
+            # nested_dict_statements.get(id1).get(id2) raises AttributeError
+            # if nested_dict_statements.get(id1) returns {}
 
-        # Evidence list must have length above this value to be part of the
-        # text output
-        ev_fltr = 0
+            found = []
 
-        # Checks subj=id1, obj=id2
-        if nested_dict_statements.get(id1) and \
-                nested_dict_statements.get(id1).get(id2):
-            stmts = nested_dict_statements[id1][id2]
-            logger.info('Found connection between %s and %s' % (id1, id2))
-            dir_conn_pairs.append((id1, id2, correlation, stmts))
-            output = dnf.latex_output(subj=id1, obj=id2, corr=correlation,
-                                      ev_len_fltr=ev_fltr, stmts=stmts,
-                                      ignore_str='parent')
-            f_con.write(output)
+            for subj, obj in itt.permutations((id1, id2), r=2):
+                if nested_dict_statements.get(subj) and \
+                        nested_dict_statements.get(subj).get(obj):
+                    stmts = nested_dict_statements[subj][obj]
+                    logger.info('Found direct connection between %s and %s' % (
+                        subj, obj))
+                    found.append(True)
+                    stmt_tuple = (subj, obj, correlation, stmts)
+                    dir_conn_pairs.append(stmt_tuple)
 
-            if correlation < 0:
-                dir_neg_conn_pairs.append((id1, id2, correlation, stmts))
-                f_neg_c.write(output)
-        else:
-            forward_fail = True
+                    # ToDo Should two_step_conn_pairs contain connections of
+                    # length == 2 or <= 2? ==2 for now.
+                    two_step_conn_pairs.append((subj, obj, correlation,
+                                                0, 'direct'))
+                    output = dnf.latex_output(subj=subj,
+                                              obj=obj,
+                                              corr=correlation,
+                                              ev_len_fltr=ev_fltr,
+                                              stmts=stmts,
+                                              ignore_str='parent')
+                    f_con.write(output)
+                    if correlation < 0:
+                        dir_neg_conn_pairs.append(stmt_tuple)
+                        f_neg_c.write(output)
 
-        # Checks subj=id2, obj=id1
-        if nested_dict_statements.get(id2) and \
-                nested_dict_statements.get(id2).get(id1):
-            stmts = nested_dict_statements[id2][id1]
-            logger.info('Found connection between %s and %s' % (id2, id1))
-            dir_conn_pairs.append((id2, id1, correlation, stmts))
-            output = dnf.latex_output(subj=id2, obj=id1, corr=correlation,
-                                      ev_len_fltr=ev_fltr, stmts=stmts,
-                                      ignore_str='parent')
-            f_con.write(output)
+                else:
+                    found.append(False)
 
-            if correlation < 0:
-                dir_neg_conn_pairs.append((id2, id1, correlation, stmts))
-                f_neg_c.write(output)
+                # any(found) is True if at least one connection was found and
+                # therefore "not any" is only True when no connection was found
+                if not any(found):
+                    # Check if connection A-X-B exists and save the set of
+                    # middle nodes.
+                    #
+                    # This can be built to a recursive search for n middle
+                    # nodes by inserting an 'else' at the bottom where the
+                    # neighbors of the neighbors are searched.
+                    if subj in node_set and obj in node_set:
+                        neighbor_node_connections = \
+                            set(undir_nx_graph[subj].keys()) & \
+                            set(undir_nx_graph[obj].keys())
+                        if neighbor_node_connections:
+                                logger.info('Found connection of length 2 '
+                                            'between %s and %s' % (subj, obj))
+                                two_step_conn_pairs.\
+                                    append((subj, obj, correlation,
+                                            len(neighbor_node_connections),
+                                            neighbor_node_connections))
 
-        else:
-            backward_fail = True
+                        # no connection A-B or A-X-B found
+                        else:
+                            unexplained.append([id1, id2, correlation])
 
-        # If both failed, count as unexplained
-        if forward_fail and backward_fail:
-            unexplained.append([id1, id2, correlation])
-
-    with open(args.outbasename+'_connections.csv', 'w', newline='') as csvf:
+    with open(args.outbasename+'_conn_correlations.csv', 'w', newline='')\
+            as csvf:
         wrtr = csv.writer(csvf, delimiter=',')
         wrtr.writerows(dir_conn_pairs)
 
-    with open(args.outbasename+'_neg_conn.csv', 'w', newline='') as csvf:
+    with open(args.outbasename + '_two_conn_correlations.csv', 'w', newline='')\
+            as csvf:
+        wrtr = csv.writer(csvf, delimiter=',')
+        wrtr.writerows(two_step_conn_pairs)
+
+    with open(args.outbasename+'_neg_conn_correlations.csv', 'w', newline='') \
+            as csvf:
         wrtr = csv.writer(csvf, delimiter=',')
         wrtr.writerows(dir_neg_conn_pairs)
 
-    with open(args.outbasename+'_unexplained.csv', 'w', newline='') as csvf:
+    with open(args.outbasename+'_unexpl_correlations.csv', 'w', newline='') \
+            as csvf:
         wrtr = csv.writer(csvf, delimiter=',')
         wrtr.writerows(unexplained)
 
