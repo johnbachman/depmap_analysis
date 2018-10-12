fa5230a029856bf32a1598dda60fdae8ef784c91
kkaris <karis.klas@gmail.com>
2018-10-12 10:24:24 -0400

    Rebased null_correlation_sampling onto include_rnai_correlations

--- depmap_script.py
+++ depmap_script.py
@@ -572,24 +572,27 @@ if __name__ == '__main__':
         'if you are running the script on the full data with no filters.')
     parser.add_argument('-lls', action='store_true', help='Use exactly one '
         'standard deviation of full set as lower limit.')
-    parser.add_argument('-crange', '--crispr-corr-range', default=[0.3],
+    parser.add_argument('--max-pairs', type=int, help='Limit the maximum '
+        'number of gene-gene pairs to explain. If used, the pairs used will '
+        'be sampled at random.')
+    parser.add_argument('-crange', '--crispr-corr-range', default=0.3,
                         type=float, nargs="+",
-                        help='LOWER_LIM UPPER_LIM\nTwo decimal numbers '
+                        help='LOWER_LIM UPPER_LIM | Two decimal numbers '
                              'denoting the range of correlations to consider '
                              'in the crispr data.')
-    parser.add_argument('-rrange', '--rnai-corr-range', default=[0.2],
+    parser.add_argument('-rrange', '--rnai-corr-range', default=0.2,
                         type=float, nargs="+",
                         help='LOWER_LIM UPPER_LIM\nTwo decimal numbers '
                              'denoting the range of correlations to consider '
                              'in the rnai data.')
     parser.add_argument('-cstats', '--crispr-mean-sigma', type=float, nargs=2,
                         help='-rstats <mean> <stdev> | Provide a value of the '
-                             'mean and standard deviation for the crispr data '
+                             'mean and standard deviation for the CRISPR data '
                              'instead of calculating it from the full data '
                              'set.')
     parser.add_argument('-rstats', '--rnai-mean-sigma', type=float, nargs=2,
                         help='-rstats <mean> <stdev> | Provide a value of the '
-                             'mean and standard deviation for the rnai data '
+                             'mean and standard deviation for the RNAi data '
                              'instead of calculating it from the full data '
                              'set.')
     a = parser.parse_args()
