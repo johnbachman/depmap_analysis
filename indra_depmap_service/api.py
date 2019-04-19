ad47f851948512af0842f82415d7a453a2ea2a76
kkaris <karis.klas@gmail.com>
2019-04-19 17:49:36 -0400

    Add flask app, routes, template load

--- indra_depmap_service/api.py
+++ indra_depmap_service/api.py
@@ -1,24 +1,40 @@
-import sys
 import json
-import boto3
 import logging
 import networkx as nx
-from networkx import NodeNotFound
+from os import path
+from jinja2 import Template
 from subprocess import call
 from datetime import datetime
 from flask import Flask, request, abort, Response, redirect, url_for
+
 from indra_db.util import dump_sif
+
 import depmap_network_functions as dnf
 from util.io_functions import _pickle_open, _dump_it_to_pickle
 
+app = Flask(__name__)
+
 logger = logging.getLogger('INDRA GDE API')
 
-# Need way to read/update latest sif dump of pa-statements:
-# see indra_db/indra_db/util/dump_sif.py
+HERE = path.dirname(path.abspath(__file__))
+CACHE = path.join(HERE, '_cache')
 
+INDRA_NETWORK_CACHE = path.join(CACHE, 'nx_dir_graph_db_dump_20190417.pkl')
 
 def _todays_date():
     return datetime.now().strftime('%Y%m%d')
+
+
+# Create a template object from the template file, load once
+def _load_template(fname):
+    template_path = path.join(HERE, fname)
+    with open(template_path, 'rt') as f:
+        template_str = f.read()
+        template = Template(template_str)
+    return template
+
+
+QUERY = _load_template('query.html')
 
 
 class IndraNetwork:
@@ -76,5 +92,41 @@ def load_indra_graph(graph_path, update=False):
     return indra_graph
 
 # Need way to receive user queries for network search
-
 # Need way to return results
+@app.route('/')
+@app.route('/query')
+def get_query_page():
+    """Loads the query page"""
+    return QUERY.render()
+
+
+@app.route('/query/submit', methods=['POST'])
+def process_query():
+    """Processing queries to the indra network"""
+    # Print inputs.
+    logger.info('Got model query')
+    logger.info('Args -----------')
+    logger.info(request.args)
+    logger.info('Json -----------')
+    logger.info(str(request.json))
+    logger.info('------------------')
+
+    try:
+        source = request.json['source']
+        target = request.json['target']
+        logger.info('Querying indra network for path between %s and %s' %
+                    (source, target))
+        result = indra_network.find_shortest_path(source, target)
+        if not result:
+            logger.info('Query returned with no path found')
+        res = {'result': result}
+        logger.info('Result: %s' % str(res))
+        return Response(json.dumps(res), mimetype='application/json')
+
+    except KeyError:
+        # return 400 badly formatted
+        abort(Response('Badly formatted json/missing parameters', 400))
+
+
+if __name__ == '__main__':
+    app.run()
