b19fde146376a6dff33048392ab045000b88990f
kkaris <karis.klas@gmail.com>
2019-04-23 18:00:56 -0400

    WIP add multi digraph from cache

--- indra_depmap_service/api.py
+++ indra_depmap_service/api.py
@@ -20,7 +20,7 @@ logger = logging.getLogger('INDRA GDE API')
 HERE = path.dirname(path.abspath(__file__))
 CACHE = path.join(HERE, '_cache')
 
-INDRA_NETWORK_CACHE = path.join(CACHE, 'nx_dir_graph_db_dump_20190417.pkl')
+INDRA_NETWORK_CACHE = path.join(CACHE, 'nx_multi_digraph_db_dump_20190417.pkl')
 MAX_NUM_PATH = 10
 MAX_PATH_LEN = 6
 
