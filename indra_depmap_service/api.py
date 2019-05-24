a3e0fbe561be1e126443adf9706a8eb859a114e2
kkaris <karis.klas@gmail.com>
2019-05-24 09:32:22 -0400

    Update cahce

--- indra_depmap_service/api.py
+++ indra_depmap_service/api.py
@@ -22,9 +22,10 @@ HERE = path.dirname(path.abspath(__file__))
 CACHE = path.join(HERE, '_cache')
 
 TEST_MDG_CACHE = path.join(CACHE, 'test_mdg_network.pkl')
-INDRA_MDG_CACHE = path.join(CACHE, 'nx_bs_multi_digraph_db_dump_20190417.pkl')
+INDRA_MDG_CACHE = path.join(CACHE,
+                            'nx_bs_fam_multi_digraph_db_dump_20190417.pkl')
 TEST_DG_CACHE = path.join(CACHE, 'test_dir_network.pkl')
-INDRA_DG_CACHE = path.join(CACHE, 'nx_bs_dir_graph_db_dump_20190417.pkl')
+INDRA_DG_CACHE = path.join(CACHE, 'nx_bs_fam_dir_graph_db_dump_20190417.pkl')
 
 MAX_PATHS = 100
 MAX_PATH_LEN = 4
