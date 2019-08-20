0be57acfe345c6159d53d618762271645f7749a9
kkaris <karis.klas@gmail.com>
2019-08-20 11:28:30 -0400

    Update api tests

--- indra_depmap_service/test_api.py
+++ indra_depmap_service/test_api.py
@@ -32,17 +32,19 @@ class IndraNetworkServiceTest(unittest.TestCase):
             'bsco': 0.0,
             'curated_db_only': False,
             'fplx_expand': False,
-            'k_shortest': 1
+            'k_shortest': 1,
+            'two_way': False
         }
 
         resp = self.app.post('query/submit', json=query)
-        resp_json = resp.get_json().get('result', None)
+        resp_json = resp.get_json()['result']
+        logger.info('Response: %s' % resp_json)
         assert resp.status_code == 200
         assert resp_json.keys() == {'paths_by_node_count', 'common_targets',
-                                    'common_parents', 'timeout'}
+                             'common_parents', 'timeout'}
         assert isinstance(resp_json['paths_by_node_count'], dict)
         assert isinstance(resp_json['common_targets'], list)
-        assert isinstance(resp_json['common_parents'], list)
+        assert isinstance(resp_json['common_parents'], dict)
         assert isinstance(resp_json['timeout'], bool)
 
     def test_query_endpoint(self):
@@ -52,22 +54,9 @@ class IndraNetworkServiceTest(unittest.TestCase):
         was properly formatted.
         """
 
-        query = {
-            'test': True,
-            'source': 'BRCA1',
-            'target': 'BRCA2',
-            'stmt_filter': [],
-            'edge_hash_blacklist': [],
-            'node_filter': ['hgnc', 'fplx'],
-            'node_blacklist': [],
-            'path_length': False,
-            'sign': 'no_sign',
-            'weighted': False,
-            'bsco': 0.0,
-            'curated_db_only': False,
-            'fplx_expand': False,
-            'k_shortest': 1
-        }
-
-        resp = self.app.post('query/submit', json=query)
+        resp = self.app.post('query/submit', json={'test': 'api'})
+        logger.info('api response: %s' % repr(resp))
         assert resp.status_code == 200
+        assert resp.get_json()
+        assert resp.get_json().get('result', None)
+        assert resp.get_json().get('result') == 'api test passed'
