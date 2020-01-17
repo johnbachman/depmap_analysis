2f035b0e1da9bd6914bb713ae7fb8ed98a2017d1
kkaris <karis.klas@gmail.com>
2020-01-17 07:44:57 -0500

    WIP Change script order, use mu sigma values

--- depmap_analysis/scripts/run_scripts.sh
+++ depmap_analysis/scripts/run_scripts.sh
@@ -12,55 +12,59 @@ lw_sif=db_dump_df_lite_hgnc_fplx.csv
 ll=5; ul='';
 echo "Running $ll-$ul SD"
 echo "Output goes to ${OUTDIR}/${ll}_${ul}sd/"
-python depmap_script.py -cf "${DATA_DIR}/19Q4/Achilles_gene_effect.csv" -cc "${OUTDIR}/_crispr_all_correlations.h5"\
-  -rf "${DATA_DIR}/demeter/D2_combined_gene_dep_scores.csv" -rc "${OUTDIR}/_rnai_all_correlations.h5"\
+python depmap_script.py -cf "${DATA_DIR}/19Q4/Achilles_gene_effect.csv" -cc "${DATA_DIR}/19Q4/_crispr_all_correlations.h5"\
+  -rf "${DATA_DIR}/demeter/D2_combined_gene_dep_scores.csv" -rc "${DATA_DIR}/demeter/_rnai_all_correlations.h5"\
   -b "${SIF_DIR}/belief_dict.pkl" --filter-type None --no-web-files\
+  --hgnc-name2id "${SIF_DIR}/hgnc_name2id.pkl" --fplx-name2id "${SIF_DIR}/fplx_name2id.pkl"\
   --explained-set /home/klas/repos/depmap_analysis/input_data/depmap/gene_sets/mito_function_depmap.csv gene\
-  -lw "${SIF_DIR}/db_dump_df_lite.csv" -cstats 0.003076 0.056813 -rstats 0.006854 0.077614 -crange $ll $ul -rrange $ll $ul\
-  -o "${OUTDIR}/${ll}_${ul}sd/" > "${OUTDIR}/${ll}_${ul}sd/stout.log"
-echo "Finished running $ll-$ul SD"
-
-ll=2; ul=3;
-echo "Running $ll-$ul SD"
-echo "Output goes to ${OUTDIR}/${ll}_${ul}sd/"
-python depmap_script.py -cf "${DATA_DIR}/19Q4/Achilles_gene_effect.csv" -cc "${OUTDIR}/_crispr_all_correlations.h5"\
-  -rf "${DATA_DIR}/demeter/D2_combined_gene_dep_scores.csv" -rc "${OUTDIR}/_rnai_all_correlations.h5"\
-  -b "${SIF_DIR}/belief_dict.pkl" --filter-type None --no-web-files\
-  --explained-set /home/klas/repos/depmap_analysis/input_data/depmap/gene_sets/mito_function_depmap.csv gene\
-  -lw "${SIF_DIR}/db_dump_df_lite.csv" -cstats 0.003076 0.056813 -rstats 0.006854 0.077614 -crange $ll $ul -rrange $ll $ul\
-  -o "${OUTDIR}/${ll}_${ul}sd/" > "${OUTDIR}/${ll}_${ul}sd/stout.log"
-echo "Finished running $ll-$ul SD"
-
-ll=3; ul=4;
-echo "Running $ll-$ul SD"
-echo "Output goes to ${OUTDIR}/${ll}_${ul}sd/"
-python depmap_script.py -cf "${DATA_DIR}/19Q4/Achilles_gene_effect.csv" -cc "${OUTDIR}/_crispr_all_correlations.h5"\
-  -rf "${DATA_DIR}/demeter/D2_combined_gene_dep_scores.csv" -rc "${OUTDIR}/_rnai_all_correlations.h5"\
-  -b "${SIF_DIR}/belief_dict.pkl" --filter-type None --no-web-files\
-  --explained-set /home/klas/repos/depmap_analysis/input_data/depmap/gene_sets/mito_function_depmap.csv gene\
-  -lw "${SIF_DIR}/db_dump_df_lite.csv" -cstats 0.003076 0.056813 -rstats 0.006854 0.077614 -crange $ll $ul -rrange $ll $ul\
+  -lw "${SIF_DIR}/${lw_sif}" -cstats $cmu $csig -rstats $rmu $rsig -crange $ll $ul -rrange $ll $ul\
   -o "${OUTDIR}/${ll}_${ul}sd/" > "${OUTDIR}/${ll}_${ul}sd/stout.log"
 echo "Finished running $ll-$ul SD"
 
 ll=4; ul=5;
 echo "Running $ll-$ul SD"
 echo "Output goes to ${OUTDIR}/${ll}_${ul}sd/"
-python depmap_script.py -cf "${DATA_DIR}/19Q4/Achilles_gene_effect.csv" -cc "${OUTDIR}/_crispr_all_correlations.h5"\
-  -rf "${DATA_DIR}/demeter/D2_combined_gene_dep_scores.csv" -rc "${OUTDIR}/_rnai_all_correlations.h5"\
+python depmap_script.py -cf "${DATA_DIR}/19Q4/Achilles_gene_effect.csv" -cc "${DATA_DIR}/19Q4/_crispr_all_correlations.h5"\
+  -rf "${DATA_DIR}/demeter/D2_combined_gene_dep_scores.csv" -rc "${DATA_DIR}/demeter/_rnai_all_correlations.h5"\
   -b "${SIF_DIR}/belief_dict.pkl" --filter-type None --no-web-files\
+  --hgnc-name2id "${SIF_DIR}/hgnc_name2id.pkl" --fplx-name2id "${SIF_DIR}/fplx_name2id.pkl"\
   --explained-set /home/klas/repos/depmap_analysis/input_data/depmap/gene_sets/mito_function_depmap.csv gene\
-  -lw "${SIF_DIR}/db_dump_df_lite.csv" -cstats 0.003076 0.056813 -rstats 0.006854 0.077614 -crange $ll $ul -rrange $ll $ul\
+  -lw "${SIF_DIR}/${lw_sif}" -cstats $cmu $csig -rstats $rmu $rsig -crange $ll $ul -rrange $ll $ul\
   -o "${OUTDIR}/${ll}_${ul}sd/" > "${OUTDIR}/${ll}_${ul}sd/stout.log"
 echo "Finished running $ll-$ul SD"
 
-ll=5;
+ll=3; ul=4;
 echo "Running $ll-$ul SD"
 echo "Output goes to ${OUTDIR}/${ll}_${ul}sd/"
-python depmap_script.py -cf "${DATA_DIR}/19Q4/Achilles_gene_effect.csv" -cc "${OUTDIR}/_crispr_all_correlations.h5"\
-  -rf "${DATA_DIR}/demeter/D2_combined_gene_dep_scores.csv" -rc "${OUTDIR}/_rnai_all_correlations.h5"\
+python depmap_script.py -cf "${DATA_DIR}/19Q4/Achilles_gene_effect.csv" -cc "${DATA_DIR}/19Q4/_crispr_all_correlations.h5"\
+  -rf "${DATA_DIR}/demeter/D2_combined_gene_dep_scores.csv" -rc "${DATA_DIR}/demeter/_rnai_all_correlations.h5"\
   -b "${SIF_DIR}/belief_dict.pkl" --filter-type None --no-web-files\
+  --hgnc-name2id "${SIF_DIR}/hgnc_name2id.pkl" --fplx-name2id "${SIF_DIR}/fplx_name2id.pkl"\
   --explained-set /home/klas/repos/depmap_analysis/input_data/depmap/gene_sets/mito_function_depmap.csv gene\
-  -lw "${SIF_DIR}/db_dump_df_lite.csv" -cstats 0.003076 0.056813 -rstats 0.006854 0.077614 -crange $ll $ul -rrange $ll $ul\
+  -lw "${SIF_DIR}/${lw_sif}" -cstats $cmu $csig -rstats $rmu $rsig -crange $ll $ul -rrange $ll $ul\
   -o "${OUTDIR}/${ll}_${ul}sd/" > "${OUTDIR}/${ll}_${ul}sd/stout.log"
 echo "Finished running $ll-$ul SD"
 
+ll=2; ul=3;
+echo "Running $ll-$ul SD"
+echo "Output goes to ${OUTDIR}/${ll}_${ul}sd/"
+python depmap_script.py -cf "${DATA_DIR}/19Q4/Achilles_gene_effect.csv" -cc "${DATA_DIR}/19Q4/_crispr_all_correlations.h5"\
+  -rf "${DATA_DIR}/demeter/D2_combined_gene_dep_scores.csv" -rc "${DATA_DIR}/demeter/_rnai_all_correlations.h5"\
+  -b "${SIF_DIR}/belief_dict.pkl" --filter-type None --no-web-files\
+  --hgnc-name2id "${SIF_DIR}/hgnc_name2id.pkl" --fplx-name2id "${SIF_DIR}/fplx_name2id.pkl"\
+  --explained-set /home/klas/repos/depmap_analysis/input_data/depmap/gene_sets/mito_function_depmap.csv gene\
+  -lw "${SIF_DIR}/${lw_sif}" -cstats $cmu $csig -rstats $rmu $rsig -crange $ll $ul -rrange $ll $ul\
+  -o "${OUTDIR}/${ll}_${ul}sd/" > "${OUTDIR}/${ll}_${ul}sd/stout.log"
+echo "Finished running $ll-$ul SD"
+
+ll=1; ul=2;
+echo "Running $ll-$ul SD"
+echo "Output goes to ${OUTDIR}/${ll}_${ul}sd/"
+python depmap_script.py -cf "${DATA_DIR}/19Q4/Achilles_gene_effect.csv" -cc "${DATA_DIR}/19Q4/_crispr_all_correlations.h5"\
+  -rf "${DATA_DIR}/demeter/D2_combined_gene_dep_scores.csv" -rc "${DATA_DIR}/demeter/_rnai_all_correlations.h5"\
+  -b "${SIF_DIR}/belief_dict.pkl" --filter-type None --no-web-files\
+  --hgnc-name2id "${SIF_DIR}/hgnc_name2id.pkl" --fplx-name2id "${SIF_DIR}/fplx_name2id.pkl"\
+  --explained-set /home/klas/repos/depmap_analysis/input_data/depmap/gene_sets/mito_function_depmap.csv gene\
+  -lw "${SIF_DIR}/${lw_sif}" -cstats $cmu $csig -rstats $rmu $rsig -crange $ll $ul -rrange $ll $ul\
+  -o "${OUTDIR}/${ll}_${ul}sd/" > "${OUTDIR}/${ll}_${ul}sd/stout.log"
+echo "Finished running $ll-$ul SD"
