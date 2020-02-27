9ee511badc62f8db1050e5fc7eedc8698b4031ec
kkaris <karis.klas@gmail.com>
2020-02-27 14:28:35 -0500

    WIP Update stats Class, skeleton for script2

---
+++ depmap_analysis/scripts/depmap_script2.py
@@ -0,0 +1,234 @@
+"""The DepMap script
+
+The script matches observed correlations with different graph represenations
+if the IndraNetwork
+
+# Inputs:
+#   1. Pre-processed correlation matrix with only (gene, gene, z-score)
+#   2. nx.DiGraph (or nx.MultiDiGraph?) of IndraNetwork containing at least
+#      agA/B: (name, ns, id), hash, type, belief, sign
+
+# Processing:
+#   0. If signed graph: match edge sign with correlation sign
+#   1. Find direct links both ways
+#   2. Find A-X-B links: both ways (2), common target, common regulator
+#   3. Find famplex links (have common parent)
+
+# Questions:
+#   Q1. Where would the cutting down to specific SD ranges be done?
+#   A: Probably outside match correlations, somewhere inside or after
+#      preprocessing. Better to do it all at once for one
+
+# Output:
+#   An object of a new class that wraps a dataframe that can generate
+#   different explanations statistics
+"""
+import numpy as np
+import pandas as pd
+import networkx as nx
+from datetime import datetime
+from depmap_analysis.network_functions.net_functions import SIGNS_TO_INT_SIGN
+from depmap_analysis.network_functions.depmap_network_functions import \
+    corr_matrix_to_generator, same_sign, get_sign
+from depmap_analysis.util.statistics import DepMapExplainer
+
+
+def match_correlations(corr_z, indranet, **kwargs):
+    """The main loop for matching correlations with INDRA explanations
+
+    Parameters
+    ----------
+    corr_z : pd.DataFrame
+        The pre-processed correlation matrix. No more processing of the
+        matrix should have to be done here, i.e. it should already have
+        filtered the correlations to the proper SD ranges and removed the
+        genes that are not applicable for this explanations
+    indranet : nx.DiGraph
+        The graph representation of the indra network. Each edge should
+        have an attribute named 'statements' containing a list of sources
+        supporting that edge. If signed search, indranet is expected to be an
+        nx.MultiDiGraph with edges keyes by (gene, gene, sign) tuples.
+
+    Returns
+    -------
+    depmap_explainer
+        An instance of the DepMapExplainer class containing the explanations
+        for the correlations.
+    """
+    min_columns = ('agA', 'agB', 'z-score')
+    id_columns = min_columns + ('agA_ns', 'agA_id', 'agB_ns', 'agB_id')
+    # Map each expl type to a function that handles that explanation
+    expl_types = {'a-b': expl_ab,
+                  'b-a': expl_ba,
+                  'common parent': find_cp,
+                  'explained set': None,  # a priori explained
+                  'a-x-b': expl_axb,
+                  'b-x-a': expl_bxa,
+                  'shared regulator': get_sr,
+                  'shared target': get_st,
+                  }
+    bool_columns = ('not in graph', 'explained') + tuple(expl_types.keys())
+    stats_columns = id_columns + bool_columns
+    expl_columns = ('agA', 'agB', 'z-score', 'expl type', 'expl data')
+
+    corr_iter = corr_matrix_to_generator(corr_z)
+    signed_search = kwargs.get('signed_search', False)
+    ymd_date = kwargs['indra_datetime'] if kwargs.get('indra_datetime') else\
+        datetime.now().strftime('%Y%m%d')
+    explainer = DepMapExplainer(stats_columns=stats_columns,
+                                expl_columns=expl_columns,
+                                indra_network_date=ymd_date)
+    stats_dict = {k: [] for k in stats_columns}
+    expl_dict = {k: [] for k in expl_columns}
+
+    for A, B, zsc in corr_iter:
+        # Initialize current iteration stats
+        stats = {k: False for k in bool_columns}
+
+        # Append to stats_dict
+        stats_dict['agA'].append(A)
+        stats_dict['agB'].append(B)
+        stats_dict['z-score'].append(zsc)
+
+        # Skip if A or B not in graph
+        if A not in indranet.nodes or B not in indranet.nodes:
+            for k in set(stats_dict.keys()).difference(set(min_columns)):
+                if k == 'not in graph':
+                    # Flag not in graph
+                    stats_dict[k].append(True)
+                else:
+                    # All columns are NaN's
+                    stats_dict[k].append(np.nan)
+            continue
+
+        # Get ns:id
+        a_ns, a_id, b_ns, b_id = get_ns_id(A, B, indranet)
+
+        # Append to stats dict
+        stats_dict['agA_ns'].append(a_ns)
+        stats_dict['agB_ns'].append(b_ns)
+        stats_dict['agA_id'].append(a_id)
+        stats_dict['agB_id'].append(b_id)
+
+        # If in expl set, skip other explanations
+        if kwargs.get('explained_set'):
+            if A in kwargs['explained_set'] and B in kwargs['eplained_Set']:
+                # Set explained set = True
+                stats_dict['explained set'].append(True)
+
+                # Set overall explained = True
+                stats_dict['explained'].append(True)
+
+                # All other columns to False
+                for k in set(bool_columns).difference(
+                        {'explained set', 'explained'}):
+                    stats_dict[k].append(False)
+
+                # Set explanation type and data
+                # Append to expl_dict
+                expl_dict['agA'].append(A)
+                expl_dict['agB'].append(B)
+                expl_dict['z-score'].append(zsc)
+                expl_dict['expl type'].append('explained set')
+                expl_dict['expl data'].append(np.nan)
+
+                # And skip the rest of explanations
+                continue
+
+        # Loop expl functions
+        for expl_type, expl_func in expl_types.items():
+            # Function signature: s, o, corr, net, signed, **kwargs
+            # Function should return what will be kept in the 'expl_data'
+            # column of the expl_df
+
+            # Skip if 'explained set', which is caught above
+            if expl_type == 'explained set':
+                continue
+
+            # Some functions reverses A, B hence the s, o assignment
+            s, o, expl_data = expl_func(A, B, zsc, indranet, signed_search,
+                                        **kwargs)
+
+            expl_dict['agA'].append(s)
+            expl_dict['agB'].append(o)
+            expl_dict['z-score'].append(zsc)
+
+            stats[expl_type] = bool(expl_data)
+
+        # Set explained column
+        stats['explained'] = any([b for b in stats.values()])
+
+        # for
+
+        # Assert that all columns are the same length
+        if not all(len(ls) for ls in stats_dict.values()):
+            raise IndexError('Unequal column lengths in stats_dict after '
+                             'iteration')
+
+    explainer.stats_df.append(other=pd.DataFrame(data=stats_dict))
+    explainer.expl_df.append(other=pd.DataFrame(data=expl_dict))
+    explainer.has_data = True
+    return explainer
+
+
+def find_cp(s, o, corr, net, signed, **kwargs):
+    pass
+
+
+def expl_axb(s, o, corr, net, signed, **kwargs):
+    pass
+
+
+def expl_bxa(s, o, corr, net, signed, **kwargs):
+    pass
+
+
+def get_sr(s, o, corr, net, signed, **kwargs):
+    pass
+
+
+def get_st(s, o, corr, net, signed, **kwargs):
+    pass
+
+
+def expl_ab(s, o, corr, net, signed, **kwargs):
+    pass
+
+
+def expl_ba(s, o, corr, net, signed, **kwargs):
+    pass
+
+
+def get_edge_statements(s, o, corr, net, signed, **kwargs):
+    if signed:
+        corr_sign = SIGNS_TO_INT_SIGN[get_sign(corr)]
+        return net.edges.get((s, o, corr_sign), None)
+    else:
+        return net.edges.get((s, o))
+
+
+def get_ns_id(subj, obj, net):
+    """Get ns:id for both subj and obj
+
+    Parameters
+    ----------
+
+    subj : str
+        The subject node
+    obj : str
+        The source node
+    net : nx.Graph
+        A networkx graph object that at least contains node entries.
+
+    Returns
+    -------
+    tuple
+        A tuple with four entries:
+        (subj namespace, subj id, obj namespace, obj id)
+    """
+    s_ns = net.nodes[subj]['ns'] if net.nodes.get(subj) else None
+    s_id = net.nodes[subj]['id'] if net.nodes.get(subj) else None
+    o_ns = net.nodes[obj]['ns'] if net.nodes.get(obj) else None
+    o_id = net.nodes[obj]['id'] if net.nodes.get(obj) else None
+
+    return s_ns, s_id, o_ns, o_id
--- depmap_analysis/util/statistics.py
+++ depmap_analysis/util/statistics.py
@@ -2,15 +2,15 @@ import pandas as pd
 
 
 class DepMapExplainer:
-    stats_columns = ('agA', 'agA_ns', 'agA_id', 'agB', 'agB_ns', 'agB_id',
-                     'z-score', 'explained', 'direct', 'common parent',
-                     'explained set', 'a-x-b', 'b-x-a', 'shared regulator',
-                     'shared target', )
-    expl_columns = ('agA', 'agB', 'z-score', 'expl_type', 'expl_data')
 
-    def __init__(self, indra_network_date, tag=None, network_type='digraph'):
+    def __init__(self, stats_columns, expl_columns, indra_network_date,
+                 tag=None, network_type='digraph'):
         self.tag = tag if tag else None
         self.indra_network_date = indra_network_date
         self.network_type = network_type
-        self.stats_df = pd.DataFrame(columns=self.stats_columns)
-        self.expl_df = pd.DataFrame(columns=self.expl_columns)
+        self.stats_df = pd.DataFrame(columns=stats_columns)
+        self.expl_df = pd.DataFrame(columns=expl_columns)
+        self.has_data = False
+        self.is_signed = True if network_type in {'signed', 'pybel'} else False
+
+    # todo add methods for statistics output
