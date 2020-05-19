96ad6b51282439c19430e633f0461b940c3f8c8c
kkaris <karis.klas@gmail.com>
2020-05-19 12:48:04 -0400

    Make node/path sign helpers independent

--- depmap_analysis/network_functions/net_functions.py
+++ depmap_analysis/network_functions/net_functions.py
@@ -464,7 +464,7 @@ def path_sign_to_signed_nodes(source, target, edge_sign):
         The source node
     target : str|int
         The target node
-    edge_sign : Union(0, 1, '+', '-', 'plus', 'minus')
+    edge_sign : int
         The sign of the edge
 
     Returns
@@ -472,39 +472,38 @@ def path_sign_to_signed_nodes(source, target, edge_sign):
     sign_tuple : (a, sign), (b, sign)
         Tuple of tuples of the valid combination of signed nodes
     """
+    # Sign definitions: + == 0, - == 1
     # + path -> (a+, b+)
     # - path -> (a+, b-)
     # (a-, b-) and (a-, b+) are also technically valid but not in this context
-    if SIGN_TO_STANDARD.get(edge_sign):
-        if SIGN_TO_STANDARD[edge_sign] == '+':
-            return (source, edge_sign), (target, edge_sign)
-        elif SIGN_TO_STANDARD[edge_sign] == '-':
-            return (source, REVERSE_SIGN[edge_sign]), (target, edge_sign)
-    else:
-        logger.warning('Invalid sign %s when translating signed edge to '
-                       'signed nodes' % edge_sign)
-        return ()
+    try:
+        if int(edge_sign) == 0:
+            return (source, 0), (target, 0)
+        else:
+            return (source, 1), (target, 0)
+    except ValueError:
+        logger.warning('Invalid sign %s when translating edge sign to int'
+                       % edge_sign)
+        return (None, None), (None, None)
 
 
 def signed_nodes_to_signed_edge(source, target):
-    """Create the triple (node, node, sign) from a signed node pair
+    """Create the triple (node, node, sign) from a pair of signed nodes
 
     Assuming source, target forms an edge of signed nodes:
     edge = (a, sign), (b, sign), return the corresponding signed edge triple
     """
+    # Sign definitions: + == 0, - == 1
     # + edge/path -> (a+, b+) and (a-, b-)
     # - edge/path -> (a-, b+) and (a+, b-)
     source_name, source_sign = source
     target_name, target_sign = target
-    if source_sign == target_sign:
-        plus = source_sign if SIGN_TO_STANDARD[source_sign] == '+' else \
-            REVERSE_SIGN[source_sign]
-        return source_name, target_name, plus
-    elif source_sign == REVERSE_SIGN[target_sign]:
-        minus = source_sign if SIGN_TO_STANDARD[source_sign] == '-' else\
-            REVERSE_SIGN[source_sign]
-        return source_name, target_name, minus
-    else:
+    try:
+        if int(source_sign) == int(target_sign):
+            return source_name, target_name, 0
+        else:
+            return source_name, target_name, 1
+    except ValueError:
         logger.warning('Error translating signed nodes to signed edge using '
                        '(%s, %s)' % (source, target))
         return None, None, None
