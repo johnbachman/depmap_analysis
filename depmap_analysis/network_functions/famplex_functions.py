f4841e2600e5b81d7bd00c28f5196fb3546ea0db
Ben Gyori <ben.gyori@gmail.com>
2020-06-11 19:40:54 -0400

    Adapt famplex functions to ontology

--- depmap_analysis/network_functions/famplex_functions.py
+++ depmap_analysis/network_functions/famplex_functions.py
@@ -1,42 +1,39 @@
-from indra.preassembler import hierarchy_manager as hm
+from indra.ontology.bio import bio_ontology
+from indra.databases import get_identifiers_url
 
 
-def get_all_entities(eh=hm.hierarchies['entity']):
+def get_all_entities(ontology=None):
     """Get a list of all entities included in HierarchyManager['entity']
 
     Parameters
     ----------
-    eh : HierarchyManager object
-        A HierarchyManager object initialized to an entities HierarchyManager.
+    ontology : IndraOntology object
+        An IndraOntology object. Default: INDRA BioOntology
 
     Returns
     -------
     entity_list : list
         A list of namespace, id, uri_id tuples
     """
+    ontology = bio_ontology if not ontology else ontology
     ent_list = []
-    eh.initialize()
-    entities_keyed_by_parent = eh._children
-    for puri, children_uris in entities_keyed_by_parent.items():
-        pns, pid = eh.ns_id_from_uri(puri)
-        ent_list.append((pns, pid, puri))
-        ns_id_child_set = set([(*eh.ns_id_from_uri(p), p)
-                               for p in entities_keyed_by_parent[puri]])
-        for cns, cid, curi in ns_id_child_set:
-            ent_list.append((cns, cid, curi))
-
+    bio_ontology.initialize()
+    for node in ontology.nodes:
+        db_ns, db_id = ontology.get_ns_id(node)
+        if db_ns in {'FPLX', 'HGNC'}:
+            ent_list.append((db_ns, db_id, get_identifiers_url(db_ns, db_id)))
     return ent_list
 
 
-def find_parent(ho=hm.hierarchies['entity'], ns='HGNC',
+def find_parent(ontology=None, ns='HGNC',
                 id_=None, type_='all'):
     """A wrapper function for he.get_parents to make the functionality more
     clear.
 
     Parameters
     ----------
-    ho : HierarchyManager object
-        A HierarchyManager object. Default: entity hierarchy object
+    ontology : IndraOntology object
+        An IndraOntology object. Default: INDRA BioOntology
     ns : str
         namespace id. Default: HGNC
     id_ : str
@@ -51,17 +48,18 @@ def find_parent(ho=hm.hierarchies['entity'], ns='HGNC',
     set
         set of parents of database id in namespace ns
     """
-    return ho.get_parents(ho.get_uri(ns, id_), type_)
+    ontology = bio_ontology if not ontology else ontology
+    return ontology.get_parents(ns, id_)
 
 
-def common_parent(ho=hm.hierarchies['entity'], ns1='HGNC',
+def common_parent(ontology=None, ns1='HGNC',
                   id1=None, ns2='HGNC', id2=None, type_='all'):
     """Returns the set of common parents.
 
     Parameters
     ----------
-    ho : HierarchyManager object
-        A HierarchyManager object. Default: entity hierarchy object
+    ontology : IndraOntology object
+        An IndraOntology object. Default: INDRA BioOntology
     ns1 : str
         namespace id. Default: HGNC
     id1 : str
@@ -80,18 +78,20 @@ def common_parent(ho=hm.hierarchies['entity'], ns1='HGNC',
     set
         set of common parents in uri format
     """
-    return find_parent(ho, ns1, id1, type_) & find_parent(ho, ns2, id2, type_)
+    ontology = bio_ontology if not ontology else ontology
+    return find_parent(ontology, ns1, id1, type_) & \
+        find_parent(ontology, ns2, id2, type_)
 
 
-def has_common_parent(ho=hm.hierarchies['entity'], ns1='HGNC', id1=None,
+def has_common_parent(ontology=None, ns1='HGNC', id1=None,
                       ns2='HGNC', id2=None, type='all'):
 
     """Returns True if id1 and id2 has at least one common parent.
 
     Parameters
     ----------
-    ho : HierarchyManager object
-        A HierarchyManager object. Default: entity hierarchy object
+    ontology : IndraOntology object
+        An IndraOntology object. Default: INDRA BioOntology
     ns1 : str
         namespace id. Default: HGNC
     id1 : str
@@ -110,6 +110,7 @@ def has_common_parent(ho=hm.hierarchies['entity'], ns1='HGNC', id1=None,
     bool
         True if hgnc1 and hgnc2 has one or more common parents.
     """
-    return bool(common_parent(ho, ns1, id1, ns2, id2, type))
+    ontology = bio_ontology if not ontology else ontology
+    return bool(common_parent(ontology, ns1, id1, ns2, id2, type))
 
 
