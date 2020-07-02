8975cfb31c5f702cbacc4e399651389d22c20795
kkaris <karis.klas@gmail.com>
2020-07-02 17:48:37 -0400

    WIP Add reactome check

--- depmap_analysis/scripts/corr_stats_async.py
+++ depmap_analysis/scripts/corr_stats_async.py
@@ -10,8 +10,12 @@ import numpy as np
 from pybel.dsl.node_classes import CentralDogma
 
 from indra.util.multiprocessing_traceback import WrapException
+from indra.databases.hgnc_client import get_current_hgnc_id, get_uniprot_id,\
+    uniprot_ids, get_hgnc_name
 
 logger = logging.getLogger(__name__)
+
+uniprot_ids_reverse = {v: k for k, v in uniprot_ids.items()}
 
 global_results = []
 global_results_pairs = []
@@ -43,11 +47,13 @@ def error_callback(err):
 
 
 class GlobalVars(object):
-    def __init__(self, df=None, z_cm=None, sampl=10):
+    def __init__(self, df=None, z_cm=None, reactome=None, sampl=10):
         if df is not None:
             global_vars['df'] = df
         if sampl:
             global_vars['subset_size'] = sampl
+        if reactome:
+            global_vars['reactome'] = reactome
         if z_cm is not None:
             global list_of_genes
             global_vars['z_cm'] = z_cm
@@ -245,81 +251,103 @@ def get_corr_stats(so_pairs):
         global list_of_genes
         df = global_vars['df']
         z_corr = global_vars['z_cm']
+        reactome = global_vars['reactome']
         subset_size = global_vars['subset_size']
         chunk_size = max(len(list_of_genes[:]) // subset_size, 1)
 
         all_axb_corrs = []
         top_axb_corrs = []
-        azb_avg_corrs = []
-        all_azb_corrs = []
         axb_avg_corrs = []
 
+        azb_avg_corrs = []
+        all_azb_corrs = []
+
+        reactome_avg_corrs = []
+        all_reactome_corrs = []
+
         # reset counters
-        counter = Counter({'z_nans': 0,
+        counter = Counter({'r_skip': 0,
                            'z_skip': 0,
                            'x_skip': 0})
 
         for subj, obj in so_pairs:
-            ab_avg_corrs = []
-            path_rows = df[(df['agA'] == subj) &
-                           (df['agB'] == obj) &
-                           ((df['expl type'] == 'a-x-b') |
-                            (df['expl type'] == 'b-x-a') |
-                           (df['expl type'] == 'shared target'))]
-
-            # Make sure we don't double-count Xs such that X is shared target
-            # and also a pathway; also, don't include X if X = subj or obj
-            x_set = set()
-            for ix, path_row in path_rows.iterrows():
-                x_set.update([x for x in path_row['expl data']
-                              if x not in (subj, obj)])
-
-            for x in x_set:
-                x_name = x.name if isinstance(x, CentralDogma) else x
-                if x_name in z_corr.columns:
-                    ax_corr = z_corr.loc[subj, x_name]
-                    xb_corr = z_corr.loc[x_name, obj]
-                    all_axb_corrs += [ax_corr, xb_corr]
-                    avg_corr = 0.5 * abs(ax_corr) + 0.5 * abs(xb_corr)
-                    ab_avg_corrs.append(avg_corr)
-                    axb_avg_corrs.append(avg_corr)
-                else:
-                    counter.update('x_skip')
-                    continue
-
-            # Get a random subset of the possible correlation z scores
-            for z in np.random.choice(list_of_genes[:], chunk_size, False):
-                try:
-                    if z == subj or z == obj:
-                        counter.update('z_skip')
-                        continue
-                    az_corr = z_corr.loc[z, subj]
-                    bz_corr = z_corr.loc[z, obj]
-                    if np.isnan(az_corr) or np.isnan(bz_corr):
-                        # Is there a more efficient way of doing this?
-                        logger.info(
-                            f'NaN correlations for '
-                            f'subj-z ({str(subj)}-{str(z)}) or '
-                            f'obj-z ({str(obj)}-{str(z)})'
-                        )
-                        counter.update('z_nans')
-                        continue
-                    all_azb_corrs.extend([az_corr, bz_corr])
-                    azb_avg_corrs.append(0.5 * abs(az_corr) + 0.5 * abs(bz_corr))
-                except KeyError as err:
-                    raise KeyError(
-                        f'KeyError was raised trying to sample background '
-                        f'correlation distribution with subject {str(subj)}'
-                        f'({subj.__class__}), object {str(obj)} '
-                        f'({obj.__class__}) and z {z} ({z.__class__})'
-                    ) from err
-            # if warn:
-            #     logger.warning('%d missing X genes out of %d in correlation '
-            #                    'matrices' % (warn, len(x_list)))
-            if len(ab_avg_corrs) > 0:
-                max_magn_avg = max(ab_avg_corrs)
+            # Get x values
+            (avg_x_corrs_per_ab, axb_corrs), x_len = \
+                get_interm_corr_stats_x(subj, obj, z_corr, df)
+            all_axb_corrs += axb_corrs
+            if len(avg_x_corrs_per_ab) > 0:
+                max_magn_avg = max(avg_x_corrs_per_ab)
+                axb_avg_corrs += avg_x_corrs_per_ab
                 top_axb_corrs.append((subj, obj, max_magn_avg))
-                #ab_to_axb_avg.append((subj, obj, comb_zsc, max_magn_avg))
+            counter['x_skip'] += x_len - len(avg_x_corrs_per_ab)
+
+            # Get z values
+            z_iter = np.random.choice(list_of_genes[:], chunk_size, False)
+            avg_z_corrs_per_ab, azb_corrs = \
+                get_interm_corr_stats_z(subj, obj, z_iter, z_corr)
+            azb_avg_corrs += avg_z_corrs_per_ab
+            all_azb_corrs += azb_corrs
+            counter['z_skip'] += len(z_iter) - len(avg_z_corrs_per_ab)
+
+            # Get reactome values
+            (avg_reactome_corrs_per_ab, reactome_corrs), r_len = \
+                get_interm_corr_stats_reactome(subj, obj, reactome, z_corr)
+            reactome_avg_corrs += avg_reactome_corrs_per_ab
+            counter['r_skip'] += r_len - len(avg_reactome_corrs_per_ab)
+
+            ## OLD ##
+
+            # path_rows = df[(df['agA'] == subj) &
+            #                (df['agB'] == obj) &
+            #                ((df['expl type'] == 'a-x-b') |
+            #                 (df['expl type'] == 'b-x-a') |
+            #                (df['expl type'] == 'shared target'))]
+            #
+            # # Make sure we don't double-count Xs such that X is shared target
+            # # and also a pathway; also, don't include X if X = subj or obj
+            # x_set = set()
+            # for ix, path_row in path_rows.iterrows():
+            #     x_set.update([x for x in path_row['expl data']
+            #                   if x not in (subj, obj)])
+            #
+            # for x in x_set:
+            #     x_name = x.name if isinstance(x, CentralDogma) else x
+            #     if x_name in z_corr.columns:
+            #         ax_corr = z_corr.loc[subj, x_name]
+            #         xb_corr = z_corr.loc[x_name, obj]
+            #         all_axb_corrs += [ax_corr, xb_corr]
+            #         avg_corr = 0.5 * abs(ax_corr) + 0.5 * abs(xb_corr)
+            #         avg_x_corrs_per_ab.append(avg_corr)
+            #     else:
+            #         continue
+            #
+            # # Get a random subset of the possible correlation z scores
+            # for z in np.random.choice(list_of_genes[:], chunk_size, False):
+            #     try:
+            #         if z == subj or z == obj:
+            #             counter.update('z_skip')
+            #             continue
+            #         az_corr = z_corr.loc[z, subj]
+            #         bz_corr = z_corr.loc[z, obj]
+            #         if np.isnan(az_corr) or np.isnan(bz_corr):
+            #             # Is there a more efficient way of doing this?
+            #             logger.info(
+            #                 f'NaN correlations for '
+            #                 f'subj-z ({str(subj)}-{str(z)}) or '
+            #                 f'obj-z ({str(obj)}-{str(z)})'
+            #             )
+            #             continue
+            #         all_azb_corrs.extend([az_corr, bz_corr])
+            #         azb_avg_corrs.append(0.5*abs(az_corr) + 0.5*abs(bz_corr))
+            #     except KeyError as err:
+            #         raise KeyError(
+            #             f'KeyError was raised trying to sample background '
+            #             f'correlation distribution with subject {str(subj)}'
+            #             f'({subj.__class__}), object {str(obj)} '
+            #             f'({obj.__class__}) and z {z} ({z.__class__})'
+            #         ) from err
+
+        # TODO Add reactome output to returned dict
         assert all(len(cl) for cl in [all_axb_corrs, axb_avg_corrs,
                                       top_axb_corrs, all_azb_corrs,
                                       azb_avg_corrs]),\
@@ -341,3 +369,93 @@ def get_corr_stats(so_pairs):
                 'azb_avg_corrs': azb_avg_corrs}
     except Exception:
         raise WrapException()
+
+
+def get_interm_corr_stats_x(subj, obj, z_corr, df):
+    path_rows = df[(df['agA'] == subj) &
+                   (df['agB'] == obj) &
+                   ((df['expl type'] == 'a-x-b') |
+                    (df['expl type'] == 'b-x-a') |
+                    (df['expl type'] == 'shared target'))]
+    x_set = set()
+    for ix, path_row in path_rows.iterrows():
+        x_set.update([x for x in path_row['expl data']
+                      if x not in (subj, obj)])
+    return _get_interm_corr_stats(subj, obj, x_set, z_corr), len(x_set)
+
+
+def get_interm_corr_stats_z(subj, obj, z_set, z_corr):
+    return _get_interm_corr_stats(subj, obj, z_set, z_corr)
+
+
+def get_interm_corr_stats_reactome(subj, obj, reactome, z_corr):
+    pathways_by_gene, genes_by_pathway, _ = reactome
+    subj_up = _hgncsym2up(subj)
+    if subj_up is None:
+        return [], []
+    obj_up = _hgncsym2up(obj)
+    if obj_up is None:
+        return [], []
+
+    paths = set(pathways_by_gene[subj_up]) & set(pathways_by_gene[obj_up])
+    gene_set = set()
+    for rp in paths:
+        gene_set.update([g for g in genes_by_pathway[rp]])
+    hgnc_gene_set = [_up2hgncsym(up) for up in gene_set]
+    return _get_interm_corr_stats(subj, obj, hgnc_gene_set, z_corr),\
+           len(hgnc_gene_set)
+
+
+def _get_interm_corr_stats(a, b, y_set, z_corr):
+    # Get a list of the maximum ax-bx average per pair
+    avg_y_corrs_per_ab = []
+
+    # Get all ax and bc correlations
+    all_ayb_corrs = []
+
+    for y in y_set:
+        try:
+            # Skip self correlations and non-existing names
+            if y is None or y == a or y == b or y not in z_corr.columns:
+                continue
+            ay_corr = z_corr.loc[y, a]
+            by_corr = z_corr.loc[y, b]
+            if np.isnan(ay_corr) or np.isnan(by_corr):
+                # Is there a more efficient way of doing this?
+                logger.info(
+                    f'NaN correlations for subj-y ({str(a)}-{str(y)}) or '
+                    f'obj-y ({str(b)}-{str(y)})'
+                )
+                continue
+
+            all_ayb_corrs += [ay_corr, by_corr]
+            avg_y_corrs_per_ab.append(0.5*(abs(ay_corr) + abs(by_corr)))
+
+        except KeyError as ke:
+            raise KeyError(
+                f'KeyError was raised trying to sample '
+                f'correlation distribution with subject {str(a)}'
+                f'({a.__class__}), object {str(b)} '
+                f'({b.__class__}) and intermediate {str(y)} ({y.__class__})'
+            ) from ke
+    return avg_y_corrs_per_ab, all_ayb_corrs
+
+
+def _hgncsym2up(hgnc_symb):
+    hgnc_id = get_current_hgnc_id(hgnc_symb)
+    if isinstance(hgnc_id, list):
+        ix = 0
+        upid = None
+        while upid is None:
+            try:
+                upid = get_uniprot_id(hgnc_id[ix])
+            except IndexError:
+                break
+            ix += 1
+    else:
+        upid = get_uniprot_id(hgnc_id)
+    return upid
+
+
+def _up2hgncsym(up_id):
+    return get_hgnc_name(uniprot_ids_reverse(up_id)) or None
