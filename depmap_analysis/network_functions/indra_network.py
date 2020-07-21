44bbf20d51de3c0eecfc8726f905b5eff9bafd6b
kkaris <karis.klas@gmail.com>
2020-07-21 13:30:43 -0400

    Add docstring in get signed node

--- depmap_analysis/network_functions/indra_network.py
+++ depmap_analysis/network_functions/indra_network.py
@@ -1603,7 +1603,16 @@ def list_all_hashes(ksp_results):
 
 
 def get_signed_node(node, sign, reverse):
-    """Given sign and direction, return a node"""
+    """Given sign and direction, return a node
+
+    Assign the correct sign to the source node:
+    If search is downstream, source is the first node and the search must
+    always start with + as node sign. The leaf node sign (i.e. the end of
+    the path) in this case will then be determined by the requested sign.
+
+    If reversed search, the source is the last node and can have
+    + or - as node sign depending on the requested sign.
+    """
     if sign is None:
         return node
     else:
