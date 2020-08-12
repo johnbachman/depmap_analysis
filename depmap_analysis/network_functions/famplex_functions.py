8129aa7d09cd06c4ed80d8bc9aaa711cd17d5087
kkaris <karis.klas@gmail.com>
2020-08-12 17:22:10 -0400

    Update find parent with immedite parents only option

--- depmap_analysis/network_functions/famplex_functions.py
+++ depmap_analysis/network_functions/famplex_functions.py
@@ -29,7 +29,7 @@ def get_all_entities(ontology=None):
     return ent_list
 
 
-def find_parent(ontology=None, ns='HGNC', id_=None):
+def find_parent(ontology=None, ns='HGNC', id_=None, immediate_only=False):
     """A wrapper function for IndraOntology.get_parents()
 
     Parameters
@@ -40,6 +40,8 @@ def find_parent(ontology=None, ns='HGNC', id_=None):
         namespace id. Default: HGNC
     id_ : str
         id to check parents for. Default: None
+    immediate_only : bool
+        Determines if all or just the immediate parents should be returned
 
     Returns
     -------
@@ -47,10 +49,13 @@ def find_parent(ontology=None, ns='HGNC', id_=None):
         set of parents as (namespace, identifier) tuples
     """
     ontology = bio_ontology if not ontology else ontology
+    if immediate_only:
+        return {p for p in ontology.child_rel(ns, id_, {'isa', 'partof'})}
     return set(ontology.get_parents(ns, id_))
 
 
-def common_parent(ontology=None, ns1='HGNC', id1=None, ns2='HGNC', id2=None):
+def common_parent(ontology=None, ns1='HGNC', id1=None, ns2='HGNC', id2=None,
+                  immediate_only=False):
     """Returns the set of common parents.
 
     Parameters
@@ -65,6 +70,8 @@ def common_parent(ontology=None, ns1='HGNC', id1=None, ns2='HGNC', id2=None):
         namespace id. Default: HGNC
     id2 : str
         Second id to check parents for. Default: None
+    immediate_only : bool
+        Determines if all or just the immediate parents should be returned
 
     Returns
     -------
@@ -72,8 +79,8 @@ def common_parent(ontology=None, ns1='HGNC', id1=None, ns2='HGNC', id2=None):
         set of common parents as (namespace, identifier) tuples
     """
     ontology = bio_ontology if not ontology else ontology
-    return find_parent(ontology, ns1, id1) & \
-        find_parent(ontology, ns2, id2)
+    return find_parent(ontology, ns1, id1, immediate_only) & \
+        find_parent(ontology, ns2, id2, immediate_only)
 
 
 def has_common_parent(ontology=None, ns1='HGNC', id1=None,
