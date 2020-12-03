70b64a15ea503031c775def5db7c08c6e471f1ea
kkaris <karis.klas@gmail.com>
2020-12-03 16:02:21 -0500

    Include pairwise jaccard data

--- depmap_analysis/scripts/drug_script.py
+++ depmap_analysis/scripts/drug_script.py
@@ -11,22 +11,41 @@ from depmap_analysis.util.statistics import DepMapExplainer
 logger = logging.getLogger(__name__)
 
 
-def get_rankings(expl_df: DataFrame, sampl_size: int = None)\
-        -> Tuple[Dict[str, Counter], Dict[str, List[Tuple[int, int, float]]]]:
+def get_rankings(expl_df: DataFrame, stats_df: DataFrame,
+                 sampl_size: int = None) -> \
+        Tuple[Dict[str, Counter],
+              Dict[str, List[Tuple[int, int, float]]],
+              List[Tuple[str, str, float, float, int, int]]]:
     """Get the count of next nearest neighborhood
 
     Parameters
     ----------
     expl_df : DataFrame
         Explanation dataframe
+    stats_df : DataFrame
+        Statistics dataframe
     sampl_size : int
-        Sampling size.
+        Sampling size
 
     Returns
     -------
     dict
         A dict of Counters for each examined agent
     """
+    jaccard_ranks = [
+        (agA, agB, corr, len(ins)/len(uni), len(ins), len(uni))
+        for agA, agB, corr, expl_type, (ins, uni) in
+        expl_df[expl_df['expl type'] ==
+                'shared downstream'].itertuples(index=False) if agA != agB
+    ]
+
+    # agA, agB, z-score, agA_ns, agA_id, agB_ns, agB_id, not in graph,
+    # explained, shared downstream
+    for (agA, agB, corr, _, _, _, _, _, _, _) in \
+            stats_df[stats_df.explained == False].itertuples(index=False):
+        if agA != agB:
+            jaccard_ranks.append((agA, agB, corr, 0, 0, float('nan')))
+
     # Get the agents that have any shared downstream explanations
     all_agents_sampled = \
         set(expl_df[expl_df['expl type'] == 'shared downstream'].agA.values) |\
@@ -48,7 +67,7 @@ def get_rankings(expl_df: DataFrame, sampl_size: int = None)\
             jaccard_index[ag_name].append((len(ins), len(uni),
                                            len(ins)/len(uni)))
         nnn_counters[ag_name] = Counter(ll)
-    return nnn_counters, jaccard_index
+    return nnn_counters, jaccard_index, jaccard_ranks
 
 
 if __name__ == '__main__':
@@ -59,7 +78,7 @@ if __name__ == '__main__':
         sample_size = None
     drug_expl = file_opener(drug_file)
     assert isinstance(drug_expl, DepMapExplainer)
-    rankings, ji = get_rankings(drug_expl.expl_df)
+    rankings, ji, jr = get_rankings(drug_expl.expl_df, drug_expl.stats_df)
 
     # Sum up the counters to get a full counter
     overall_ranking = Counter()
@@ -75,7 +94,15 @@ if __name__ == '__main__':
                                 sum(li)/len(li),
                                 sum(lu)/len(lu)))
     jaccard_ranking.sort(key=lambda t: t[1], reverse=True)
-    jaccard_df = DataFrame(
+    jaccard_df_per_drug = DataFrame(
         data=jaccard_ranking,
         columns=['drug', 'jaccard_index', 'n_intersection', 'n_union']
     )
+    jaccard_df_per_pair = DataFrame(
+        data=jr,
+        columns=['drugA', 'drugB', 'corr', 'jaccard_index',
+                 'n_intersection', 'n_union']
+    ).sort_values(by=['jaccard_index', 'corr'], ascending=[False, True])
+    logger.info('Done with script, results are in variables '
+                '`overall_ranking`, `jaccard_df_per_drug` and '
+                '`jaccard_df_per_pair`')
