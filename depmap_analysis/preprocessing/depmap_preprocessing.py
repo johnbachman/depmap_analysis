a93b0c4c57e6bd31679905f0f920db0085fdd005
kkaris <karis.klas@gmail.com>
2020-12-16 16:57:45 -0500

    Move script part to end of file

--- depmap_analysis/preprocessing/depmap_preprocessing.py
+++ depmap_analysis/preprocessing/depmap_preprocessing.py
@@ -156,68 +156,6 @@ def get_drug_corr_matrix(drug_resp_file, drug_info_file):
     return corr_df
 
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser('DepMap Data pre-processing')
-    # Input dirs
-    parser.add_argument('--crispr-raw',
-                        help='The raw CRISPR gene effect data. File name '
-                             'should match *gene_effect.csv')
-    parser.add_argument('--rnai-raw',
-                        help='The raw RNAi gene dependency data. File name '
-                             'should match *gene_dep_scores.csv')
-    # Option to start from raw correlations matrix instead of running
-    # correlation calculation directly
-    parser.add_argument('--crispr-corr',
-                        help='The file containing an hdf compressed '
-                             'correlation data frame of the crispr data. If '
-                             'this file is provided, the raw crispr data is '
-                             'ignored.')
-    parser.add_argument('--rnai-corr',
-                        help='The file containing an hdf compressed '
-                             'correlation data frame of the rnai data. If '
-                             'this file is provided, the raw rnai data is '
-                             'ignored.')
-    # Output dirs
-    parser.add_argument('--output-dir', '-o',
-                        help='Optional. A directory where to put the '
-                             'output of the script. If not provided the '
-                             'ouput will go to the directory/ies where '
-                             'the corresponding input came from, i.e. the '
-                             'output from the RNAi input will be placed '
-                             'in the RNAi input directory. The combined '
-                             'z-score matrix will be written to the crispr '
-                             'input directory if this option is not '
-                             'provided.')
-    parser.add_argument('--random', '-r', type=int,
-                        help='Optional. If specified, provide the size of '
-                             'the final correlation matrix where the genes '
-                             'are picked at random from the intersection of '
-                             'genes from both the RNAI and CRISPR data sets.')
-    parser.add_argument('--fname', help='A file name for the output '
-                                        'correlations DataFrame.')
-
-    args = parser.parse_args()
-    options = {
-        'crispr_raw': args.crispr_raw,
-        'rnai_raw': args.rnai_raw,
-        'crispr_corr': args.crispr_corr,
-        'rnai_corr': args.rnai_corr,
-        'output_dir': args.output_dir,
-        'random_sampl': args.random
-    }
-
-    # Run main script
-    z_corr = run_corr_merge(**options)
-
-    # Write merged correlations combined z score
-    outdir = args.output_dir if args.output_dir else (Path(
-        args.crispr_corr).parent.as_posix() if args.crispr_corr else Path(
-        args.crispr_raw).parent.as_posix())
-    logger.info(f'Writing combined correlations to {outdir}')
-    fname = args.fname if args.fname else 'combined_z_score.h5'
-    z_corr.to_hdf(Path(outdir, fname), 'zsc')
-
-
 def drugs_to_corr_matrix(raw_file: str, info_file: str):
     """Preprocess and create a correlation matrix from raw drug data"""
     def _get_drug_name(drug_id):
@@ -331,3 +269,65 @@ def merge_corr_df(corr_df, other_corr_df, remove_self_corr=True,
         dep_z = dep_z.dropna(axis=0, how='all').dropna(axis=1, how='all')
     assert dep_z.notna().sum().sum() > 0, print('Correlation matrix is empty')
     return dep_z
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser('DepMap Data pre-processing')
+    # Input dirs
+    parser.add_argument('--crispr-raw',
+                        help='The raw CRISPR gene effect data. File name '
+                             'should match *gene_effect.csv')
+    parser.add_argument('--rnai-raw',
+                        help='The raw RNAi gene dependency data. File name '
+                             'should match *gene_dep_scores.csv')
+    # Option to start from raw correlations matrix instead of running
+    # correlation calculation directly
+    parser.add_argument('--crispr-corr',
+                        help='The file containing an hdf compressed '
+                             'correlation data frame of the crispr data. If '
+                             'this file is provided, the raw crispr data is '
+                             'ignored.')
+    parser.add_argument('--rnai-corr',
+                        help='The file containing an hdf compressed '
+                             'correlation data frame of the rnai data. If '
+                             'this file is provided, the raw rnai data is '
+                             'ignored.')
+    # Output dirs
+    parser.add_argument('--output-dir', '-o',
+                        help='Optional. A directory where to put the '
+                             'output of the script. If not provided the '
+                             'ouput will go to the directory/ies where '
+                             'the corresponding input came from, i.e. the '
+                             'output from the RNAi input will be placed '
+                             'in the RNAi input directory. The combined '
+                             'z-score matrix will be written to the crispr '
+                             'input directory if this option is not '
+                             'provided.')
+    parser.add_argument('--random', '-r', type=int,
+                        help='Optional. If specified, provide the size of '
+                             'the final correlation matrix where the genes '
+                             'are picked at random from the intersection of '
+                             'genes from both the RNAI and CRISPR data sets.')
+    parser.add_argument('--fname', help='A file name for the output '
+                                        'correlations DataFrame.')
+
+    args = parser.parse_args()
+    options = {
+        'crispr_raw': args.crispr_raw,
+        'rnai_raw': args.rnai_raw,
+        'crispr_corr': args.crispr_corr,
+        'rnai_corr': args.rnai_corr,
+        'output_dir': args.output_dir,
+        'random_sampl': args.random
+    }
+
+    # Run main script
+    z_corr = run_corr_merge(**options)
+
+    # Write merged correlations combined z score
+    outdir = args.output_dir if args.output_dir else (Path(
+        args.crispr_corr).parent.as_posix() if args.crispr_corr else Path(
+        args.crispr_raw).parent.as_posix())
+    logger.info(f'Writing combined correlations to {outdir}')
+    fname = args.fname if args.fname else 'combined_z_score.h5'
+    z_corr.to_hdf(Path(outdir, fname), 'zsc')
