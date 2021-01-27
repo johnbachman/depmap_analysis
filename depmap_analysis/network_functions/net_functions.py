b7c07f267f64db69ded26e4594fb8abc3c1cb3b1
kkaris <karis.klas@gmail.com>
2021-01-27 11:21:41 -0500

    Check filtered DataFrame is not empty

--- depmap_analysis/network_functions/net_functions.py
+++ depmap_analysis/network_functions/net_functions.py
@@ -190,6 +190,13 @@ def expand_signed(df: pd.DataFrame, sign_dict: Dict[str, int],
     # Locate relevant rows
     standard_sign = df.stmt_type.isin(sign_dict.keys())
     expand_sign = df.stmt_type.isin(stmt_types)
+    assert sum(standard_sign) + sum(expand_sign) > 0, \
+        'All rows filtered out from DataFrame. Check that statement types ' \
+        'in sign_dict and stmt_types exist in the DataFrame.'
+    if sum(expand_sign) > 0:
+        logger.warning('No rows can be used for expanded signed edges. Check '
+                       'that statement types in stmt_types exist in the '
+                       'DataFrame.')
 
     # Add sign for signed statements
     logger.info('Setting initial sign for signed types')
