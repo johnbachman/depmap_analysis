4f8effebc1983341a6da3cda45a6051d243ff728
kkaris <karis.klas@gmail.com>
2021-01-29 10:45:52 -0500

    WIP Restructure setup

--- depmap_analysis/tests/test_graph_generation.py
+++ depmap_analysis/tests/test_graph_generation.py
@@ -4,19 +4,28 @@ from depmap_analysis.network_functions.net_functions import \
     sif_dump_df_to_digraph
 
 # Add input
+agA_names = ['nameX1', 'nameX2']
+agA_ns_list = ['nsX1', 'nsX2']
+agA_ids = ['idX1', 'idX2']
+agB_names = ['nameY1', 'nameY2']
+agB_ns_list = ['nsY1', 'nsY2']
+agB_ids = ['idY1', 'idY2']
 h1, h2 = 1234657890, 9876543210
 hashes = [h1, h2]
 bd = [0.685, 0.95]
+stmt_types = ['Activation', 'Complex']
+ev_counts = [7, 13]
 src = [{'srcA': 2, 'srcB': 5}, {'srcA': 5, 'srcB': 8}]
-sif = {'agA_name': ['nameX1', 'nameX2'], 'agA_ns': ['nsX1', 'nsX2'],
-       'agA_id': ['idX1', 'idX2'], 'agB_name': ['nameY1', 'nameY2'],
-       'agB_ns': ['nsY1', 'nsY2'], 'agB_id': ['idY1', 'idY2'],
-       'stmt_type': ['Activation', 'Complex'], 'evidence_count': [7, 13],
-       'stmt_hash': hashes, 'source_counts': src, 'belief': bd}
+
+sif_dict = {'agA_name': agA_names, 'agA_ns': agA_ns_list,
+            'agA_id': agA_ids, 'agB_name': agB_names, 'agB_ns': agB_ns_list,
+            'agB_id': agB_ids, 'stmt_type': stmt_types,
+            'evidence_count': ev_counts, 'stmt_hash': hashes,
+            'source_counts': src, 'belief': bd}
 
 
 def test_digraph_dump():
-    sif_df = pd.DataFrame(sif)
+    sif_df = pd.DataFrame(sif_dict)
     idg = sif_dump_df_to_digraph(df=sif_df,
                                  date=datetime.utcnow().strftime('%Y-%m-%d'),
                                  graph_type='digraph',
