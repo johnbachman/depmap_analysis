108617f35e8e5cb06fa6222f777eae9e3f14c733
kkaris <karis.klas@gmail.com>
2021-03-16 15:30:23 -0400

    Add test for sub-sampling

--- depmap_analysis/tests/test_script.py
+++ depmap_analysis/tests/test_script.py
@@ -169,6 +169,107 @@ def test_iterator_slicing():
         f'chunk_ix+1={chunk_ix + 1}, chunks_wanted={chunks_wanted}'
 
 
+def test_sampling():
+    size = 50
+    a = _gen_sym_df(size)
+
+    pairs = set()
+    n = 0
+    for n in range(size):
+        k = 0
+        row, col = _get_off_diag_pair(size)
+        while (row, col) in pairs:
+            row, col = _get_off_diag_pair(size)
+            k += 1
+            if k > 1000:
+                print('Too many while iterations, breaking')
+                break
+        if k > 1000:
+            break
+        a.iloc[row, col] = np.nan
+        a.iloc[col, row] = np.nan
+        pairs.add((row, col))
+        pairs.add((col, row))
+
+    pairs_removed = n + 1
+    # Assert that we're correct so far
+
+    # Get total pairs available:
+    total_pairs = get_pairs(a)
+
+    # all items - diagonal - all removed items off diagonal
+    assert (size**2 - size - 2*pairs_removed) / 2 == total_pairs
+
+    # Check that the iterator slicing for multiprocessing runs through all
+    # the pairs
+
+    # Test max pairs
+    max_pairs = total_pairs//2
+    total_pairs = max_pairs
+    chunks_wanted = 10
+
+    chunksize = get_chunk_size(chunks_wanted, total_pairs)
+
+    chunk_iter = batch_iter(
+        iterator=corr_matrix_to_generator(a, max_pairs=total_pairs),
+        batch_size=chunksize, return_func=list
+    )
+
+    pair_count = 0
+    chunk_ix = 0
+    for chunk_ix, list_of_pairs in enumerate(chunk_iter):
+        pair_count += len([(t[0][0], t[0][1], t[1]) for t in
+                           list_of_pairs if t is not None])
+
+    # Were all pairs looped?
+    assert pair_count == total_pairs, \
+        f'pair_count={pair_count} total_pairs={total_pairs}'
+    # Does the number of loop iterations correspond to the number of chunks
+    # wanted?
+    assert chunk_ix + 1 == chunks_wanted, \
+        f'chunk_ix+1={chunk_ix + 1}, chunks_wanted={chunks_wanted}'
+
+    # Redo the same with subset of names
+    name_subset = list(np.random.choice(a.columns.values,
+                                        size=size // 3,
+                                        replace=False))
+    # Add a name that does not exist in the original df
+    name_subset.append(size+2)
+
+    # Get total pairs available
+    total_pairs_permute = get_pairs(a, subset_list=name_subset)
+    max_pairs = total_pairs_permute // 4
+    total_pairs_permute = max_pairs
+
+    # Chunks wanted
+    chunks_wanted = 10
+
+    chunksize = get_chunk_size(chunks_wanted, total_pairs_permute)
+
+    chunk_iter = batch_iter(
+        iterator=corr_matrix_to_generator(a,
+                                          max_pairs=total_pairs_permute,
+                                          subset_list=name_subset),
+        batch_size=chunksize,
+        return_func=list
+    )
+
+    pair_count = 0
+    chunk_ix = 0
+    for chunk_ix, list_of_pairs in enumerate(chunk_iter):
+        pair_count += len([(t[0][0], t[0][1], t[1]) for t in
+                           list_of_pairs if t is not None])
+
+    # Were all pairs looped?
+    assert pair_count == total_pairs_permute, \
+        f'pair_count={pair_count} total_pairs={total_pairs_permute}'
+
+    # Does the number of loop iterations correspond to the number of chunks
+    # wanted?
+    assert chunk_ix + 1 == chunks_wanted, \
+        f'chunk_ix+1={chunk_ix + 1}, chunks_wanted={chunks_wanted}'
+
+
 def test_depmap_script():
     up2path, _, pathid2pathname = file_opener(
         's3://depmap-analysis/misc_files/reactome_pathways.pkl')
