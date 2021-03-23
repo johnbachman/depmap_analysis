ce415abca6d6c8fd17f77c1ee65fc28c0970969a
kkaris <karis.klas@gmail.com>
2021-03-23 09:28:04 -0400

    Update docstring

--- depmap_analysis/scripts/depmap_script2.py
+++ depmap_analysis/scripts/depmap_script2.py
@@ -530,11 +530,7 @@ def main(indra_net: str,
         explanation of why the entity is explained. To use the default
         MitoCarta 3.0 file, run the following code:
         >>> from depmap_analysis.scripts.depmap_script2 import mito_file
-        >>> from depmap_analysis.preprocessing import get_mitocarta_info
-        >>> apriori_mapping = get_mitocarta_info(mito_file)
-        then pass `apriori_mapping` as `apriori_explained` when calling this
-        function:
-        >>> main(apriori_explained=apriori_mapping, ...)
+        >>> main(apriori_explained=mito_file, ...)
     allowed_ns : Optional[List[str]]
         A list of allowed name spaces for explanations involving
         intermediary nodes. Default: Any namespace.
