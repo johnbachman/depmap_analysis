63d39636b3dd258ef1f9488bf491b8ec94de4e90
kkaris <karis.klas@gmail.com>
2021-07-13 10:27:24 -0400

    Use Alberts scipy function. Move functions.

--- depmap_analysis/util/statistics.py
+++ depmap_analysis/util/statistics.py
@@ -1,15 +1,19 @@
 import logging
 from math import floor
 from io import BytesIO
+from time import time
 from typing import Tuple, Dict, Union, Hashable, List, Optional, Any, \
     Iterator, Generator
 from pathlib import Path
 from datetime import datetime
 
 import boto3
+import numpy as np
 import pandas as pd
 import networkx as nx
 import matplotlib.pyplot as plt
+from scipy import stats
+from scipy.special import erfcinv, ndtri_exp
 
 from indra.util.aws import get_s3_client
 from indra_db.util.s3_path import S3Path
@@ -22,7 +26,8 @@ from depmap_analysis.post_processing import *
 logger = logging.getLogger(__name__)
 
 
-__all__ = ['min_columns', 'id_columns', 'expl_columns', 'DepMapExplainer']
+__all__ = ['min_columns', 'id_columns', 'expl_columns', 'DepMapExplainer',
+           'get_z', 'get_logp']
 
 
 min_columns = ('pair', 'agA', 'agB', 'z_score')
@@ -874,3 +879,61 @@ def _joinpath(fpath: Union[S3Path, Path], other: str) -> Union[S3Path, Path]:
         else:
             raise ValueError(f'Unable to join {fpath.to_string()} and '
                              f'{other} with "/"')
+
+
+def logerfcinv(logp):
+    """Calculate inverse of complementary error function given log of argument
+
+    """
+    return np.where(logp > -10, erfcinv(np.exp(logp)),
+                    ndtri_exp(logp))
+
+
+def norminv_logcdf(logp):
+    """Inverse CDF corresponding to log of p value"""
+    return -np.sqrt(2) * logerfcinv(np.log(2) + logp)
+
+
+def get_logp(recalculate, data_n, data_corr, filepath, method='beta'):
+    if method not in ('t', 'beta'):
+        raise ValueError('Method must be t or beta')
+    start = time()
+    if recalculate:
+        # T-statistic method
+        # See https://stackoverflow.com/a/24469099
+        # See https://support.minitab.com/en-us/minitab-express/1/help-and-how-to/basic-statistics/inference/supporting-topics/basics/manually-calculate-a-p-value/
+        if method == 't':
+            t = data_corr * np.sqrt((data_n - 2)/(1 - data_corr * data_corr))
+            logp = np.log(2) + stats.t.logsf(t.abs(), data_n-2)
+        # Beta-distribution method
+        # https://github.com/scipy/scipy/blob/v1.6.2/scipy/stats/stats.py#L3781-L3962
+        elif method == 'beta':
+            ab = data_n/2 - 1
+            logp = np.log(2) + stats.beta.logcdf(-abs(data_corr), ab, ab, loc=-1, scale=2)
+        # Make dataframe
+        data_logp = pd.DataFrame(logp, columns=data_corr.columns, index=data_corr.index)
+        data_logp.to_hdf('%s.h5' % filepath, filepath.split('/')[-1])
+    else:
+        data_logp = pd.read_hdf('%s.h5' % filepath)
+    elapsed = time() - start
+    print(elapsed, "sec")
+    return data_logp
+
+
+def get_z(recalculate, data_logp, data_corr, filepath):
+    start = time()
+    if recalculate:
+        #z_mat = stats.norm.ppf(1 - np.exp(data_logp) / 2)
+        #z_mat = -norminv_logcdf(data_logp - np.log(2))
+        z_mat = abs(norminv_logcdf(data_logp - np.log(2)))
+        data_sign = data_corr.copy()
+        data_sign[data_sign < 0] = -1
+        data_sign[data_sign > 0] = 1
+        data_z = data_sign * pd.DataFrame(z_mat, index=data_logp.columns,
+                                          columns=data_logp.columns)
+        data_z.to_hdf('%s.h5' % filepath, filepath.split('/')[-1])
+    else:
+        data_z = pd.read_hdf('%s.h5' % filepath)
+    elapsed = time() - start
+    print(elapsed, "sec")
+    return data_z
